0fb719c360844b227f2181c53b9db5e4fadb1868
Megan Arneau <megan.arneau@ekino.com>
2017-10-14 14:46:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
